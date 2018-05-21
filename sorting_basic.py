b31caff8641ef9352c32e8434d8e6088747ab53e
LZ <lizhuangneu@163.com>
2018-05-21 11:09:53 +0800

    基础排序

---
+++ sorting_basic.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : sorting_basic.py
+# @Author: 投笔从容
+# @Date  : 2018/5/21
+# @Desc  : 基础排序
+
+'''
+基础排序笔记：
+\-为什么要学习O(n^2)的排序算法
+    基础
+    编码简单易于实现
+    在一些特殊的情况下 简单的排序算法更有效
+    简单的排序思想衍生出复杂的排序算法
+    作为子过程 改进更复杂的排序算法
+\-选择排序
+
+
+
+
+'''
+
+def selectonSort(arr, n):
+
+
+
+
+if __name__ == '__main__':
+
+    a = [70, 9,8,7,6,5,4,3,2,1]
+    selectonSort(a, 10)
+    print(a)
