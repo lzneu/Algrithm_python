cfa27e425d93ef7712827b7d119c1d02fc50c3ae
LZ <lizhuangneu@163.com>
2018-06-15 22:33:19 +0800

    最长公共子串6.15提交

--- leetcode.py
+++ leetcode.py
@@ -1,28 +1,27 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        n = len(strs)
+        if n == 0:
+            return ''
+        res = 0
+        stop = False
+        m = len(strs[0])
+        for str1 in strs:
+            m = min(m, len(str1))
+        for i in range(m):
+            c = strs[0][i]
+            for j in range(1, n):
+                if c != strs[j][i]:
+                    res -= 1
+                    stop = True
+                    break
+            res += 1
+            if stop:
+                break
+        return strs[0][:res]
 
-class Solution:
-    def binaryTreePaths(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[str]
-        """
-        # 思路 返回值为 元素为字符串的list 递归过程为list中每个元素前面加上“root.val->”合并左右子树的list
-        # 终止条件
-        if root is None:
-            return []
-        # root为叶子结点时
-        if not root.left and not root.right:
-            return [str(root.val)]
-        else:  # root 为非叶子结点
-            path1 = self.binaryTreePaths(root.left)
-            path2 = self.binaryTreePaths(root.right)
-            res = []
-            for str in path1 +path2:
-                str1 = str(root.val)+'->'+str
-                res.append(str1)
-            return res
+print(Solution().longestCommonPrefix(["aca","cba"]))
--- treeNode.py
+++ treeNode.py
@@ -32,34 +32,47 @@ def createTree(lists):
             i += 1
     return root
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
-    def __findPath(self, root, sum):
+    # 返回值为 (偷该房间获得的最大money, 不偷该房间获得的最大money)
+    def __dfs(self, root):
+        money1, money2 = 0, 0
         if root is None:
-            return 0
-        res = 0
-        if root.val ==sum:
-            res += 1
-        res += self.__findPath(root.left, sum - root.val)
-        res += self.__findPath(root.right, sum - root.val)
-        return res
+            return money1, money2
 
+        # 深度优先遍历
+        res1 = self.__dfs(root.left)
+        res2 = self.__dfs(root.right)
+        money1 = root.val + res1[1] + res2[1]
+        money2 = max(res1[0], res1[1]) + max(res2[0], res2[1])
+        print('结点%d:  选%d++不选%d' % (root.val, money1, money2))
+        return money1, money2
 
-    def pathSum(self, root, sum):
+    def rob(self, root):
         """
         :type root: TreeNode
-        :type sum: int
         :rtype: int
         """
-        # 递归过程包括两个部分 一个是包含root的路径寻找 另一个是不包含root的路径寻找
-        if root is None:
-            return 0
-        # 包含root的路径
-        res = self.__findPath(root, sum)
-        # 不包含root
-        res += self.pathSum(root.left, sum)
-        res += self.pathSum(root.right, sum)
-        return res
+        # 思路 两次深度优先遍历？
+        res = self.__dfs(root)
+        return max(res[1], res[0])
 
 
-root = createTree([10,5,-3,3,2,None,11,3,-2,None,1])
-print(Solution().pathSum(root, 8))
+root = createTree([4,1,None,2,None,3])
+res = Solution().rob(root)
+print(res)
